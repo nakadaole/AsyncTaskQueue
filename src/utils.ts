1b74243b54d9318cb0be026844a927e0deccc7fa
nakadaole <genesis@nakadaole.com>
2021-07-03 01:45:00 +0800

    encapsulate redis interface

---
+++ src/utils.ts
@@ -0,0 +1,34 @@
+import client from './Client';
+
+export const getRedisValue = (key: string): Promise<string | null> => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const reply: any = await client.get(key);
+            resolve(reply);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+export const setRedisValue = (key: string, value: string) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const reply: any = await client.set(key, value);
+            resolve(reply);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+export const delRedisKey = (key: string) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const reply: any = await client.del(key);
+            resolve(reply);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
